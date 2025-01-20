d6560da1fef1cf5c088f0d0ccadbdf12471df91e
Bramhaaa <altbramhabajannavar@gmail.com>
2025-01-20 14:11:51 +0530

    First commit, only basic structure done

---
+++ portfolio_api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+# from .models import Resume
+from .serializers import ResumeSerializer
+
+# class ResumeViewSet(viewsets.ModelViewSet):
+#     queryset = Resume.objects.all()
+#     serializer_class = ResumeSerializer
